2cb4e48350e7ff02507b33b157df8e5c9b6781f7
Tanmay Sarkar <sarkartanmay393@gmail.com>
2022-06-28 13:55:09 +0530

    added elevation in home cateegories listtiles

--- lib/ui/home/widgets/categories.dart
+++ lib/ui/home/widgets/categories.dart
@@ -3,6 +3,56 @@ import 'package:flutter/material.dart';
 class Categories extends StatelessWidget {
   const Categories({Key? key}) : super(key: key);
 
+  Widget cat(String n1) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 10),
+      child: InkWell(
+        borderRadius: BorderRadius.circular(18),
+        onTap: () {},
+        child: Card(
+          elevation: 2.5,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
+          child: ListTile(
+            tileColor: Colors.red,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(18),
+            ),
+            leading: const Icon(
+              Icons.directions_bike_outlined,
+              color: Colors.white,
+            ),
+            title: Row(
+              children: [
+                Text(
+                  n1,
+                  style: TextStyle(fontSize: 13, color: Colors.white),
+                ),
+              ],
+            ),
+            trailing: Stack(
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.notifications_active_outlined),
+                  color: Colors.white,
+                ),
+                const Positioned(
+                    right: 6,
+                    top: 8,
+                    child: Icon(
+                      Icons.album_outlined,
+                      size: 8,
+                      color: Colors.lightGreenAccent,
+                    ))
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -17,174 +67,10 @@ class Categories extends StatelessWidget {
         const SizedBox(
           height: 12,
         ),
-        Container(
-          padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 10),
-          child: InkWell(
-            onTap: () {},
-            borderRadius: BorderRadius.circular(15),
-            child: ListTile(
-              tileColor: Colors.red,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(18),
-              ),
-              leading: const Icon(
-                Icons.directions_bike_outlined,
-                color: Colors.white,
-              ),
-              title: Row(
-                children: const [
-                  Text(
-                    "Bike    |  Request : 01",
-                    style: TextStyle(fontSize: 13, color: Colors.white),
-                  ),
-                ],
-              ),
-              trailing: Stack(
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.notifications_active_outlined),
-                    color: Colors.white,
-                  ),
-                  const Positioned(
-                      right: 6,
-                      top: 8,
-                      child: Icon(
-                        Icons.album_outlined,
-                        size: 8,
-                        color: Colors.lightGreenAccent,
-                      ))
-                ],
-              ),
-            ),
-          ),
-        ),
-        Container(
-          padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 10),
-          child: InkWell(
-            onTap: () {},
-            borderRadius: BorderRadius.circular(15),
-            child: ListTile(
-              tileColor: Colors.red,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(18),
-              ),
-              leading: const Icon(
-                Icons.car_crash_outlined,
-                color: Colors.white,
-              ),
-              title: Row(
-                children: const [
-                  Text(
-                    "Auto    |  Request : 01",
-                    style: TextStyle(fontSize: 13, color: Colors.white),
-                  ),
-                ],
-              ),
-              trailing: Stack(
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.notifications_active_outlined),
-                    color: Colors.white,
-                  ),
-                  const Positioned(
-                      right: 6,
-                      top: 8,
-                      child: Icon(
-                        Icons.album_outlined,
-                        size: 8,
-                        color: Colors.lightGreenAccent,
-                      ))
-                ],
-              ),
-            ),
-          ),
-        ),
-        Container(
-          padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 10),
-          child: InkWell(
-            onTap: () {},
-            borderRadius: BorderRadius.circular(15),
-            child: ListTile(
-              tileColor: Colors.red,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(18),
-              ),
-              leading: const Icon(
-                Icons.car_repair_outlined,
-                color: Colors.white,
-              ),
-              title: Row(
-                children: const [
-                  Text(
-                    "Car    |  Request : 01",
-                    style: TextStyle(fontSize: 13, color: Colors.white),
-                  ),
-                ],
-              ),
-              trailing: Stack(
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.notifications_active_outlined),
-                    color: Colors.white,
-                  ),
-                  const Positioned(
-                      right: 6,
-                      top: 8,
-                      child: Icon(
-                        Icons.album_outlined,
-                        size: 8,
-                        color: Colors.lightGreenAccent,
-                      ))
-                ],
-              ),
-            ),
-          ),
-        ),
-        Container(
-          padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 10),
-          child: InkWell(
-            onTap: () {},
-            borderRadius: BorderRadius.circular(15),
-            child: ListTile(
-              tileColor: Colors.red,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(18),
-              ),
-              leading: const Icon(
-                Icons.fire_truck_outlined,
-                color: Colors.white,
-              ),
-              title: Row(
-                children: const [
-                  Text(
-                    "Truck    |  Request : 01",
-                    style: TextStyle(fontSize: 13, color: Colors.white),
-                  ),
-                ],
-              ),
-              trailing: Stack(
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.notifications_active_outlined),
-                    color: Colors.white,
-                  ),
-                  const Positioned(
-                      right: 6,
-                      top: 8,
-                      child: Icon(
-                        Icons.album_outlined,
-                        size: 8,
-                        color: Colors.lightGreenAccent,
-                      ))
-                ],
-              ),
-            ),
-          ),
-        ),
+        cat("Bike    |  Request : 01"),
+        cat("Auto    |  Request : 01"),
+        cat("Car    |  Request : 01"),
+        cat("Truck    |  Request : 01"),
       ],
     );
   }
